e5af776218bffaeeae0c08e7f824056e5202231f
Lukas <lukas@schoenbeck.de>
2023-02-27 11:32:39 +0100

    bugfixes in compile script

--- scripts/compileall.sh
+++ scripts/compileall.sh
@@ -2,23 +2,23 @@ shopt -s nullglob
 shopt -s globstar
 main_dir="$(pwd)/.."
 
-console_output_dir="${main_dir}/compiler_output"
+console_output_dir="${main_dir}/scripts/compiler_output"
 mkdir -p $console_output_dir
 rm ${console_output_dir}/compile.out
 touch ${console_output_dir}/compile.out
 rm ${console_output_dir}/compile.err
 touch ${console_output_dir}/compile.err
 
-file_output_dir="${main_dir}/file_output"
+file_output_dir="${main_dir}/_rulepackage"
 mkdir -p $file_output_dir
 rm ${file_output_dir}/*
 
-files=(*/*.tex)
+files=(${main_dir}/*/*.tex)
 
 for i in ${files[@]}
 do
     cd ${i%/*.tex}
-    echo "${i##*/}"
+    echo "Compiling: ${i##*/}"
     lualatex.exe -synctex=1 -interaction=nonstopmode --output-directory="${file_output_dir}" ${i##*/} 1>> "${console_output_dir}/compile.out" 2>> "${console_output_dir}/compile.err"
     cd $main_dir
 done
