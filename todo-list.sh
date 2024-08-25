79d32b5baa3c9b15c5d0d9129ea41e653124b0f4
lHeidbreder <lukas@boeke.email>
2024-08-25 14:24:39 +0200

    improved todo list

--- todo-list.sh
+++ todo-list.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 shopt -s globstar
 
+SEARCHREGEX='todo|fixme|quick ?fix'
+
 if [ "$1" = "list" ]
-then grep -ri -e 'todo' **/*.{tex,csv} > "$(printf '%(%Y-%m-%d)T\n' -1).todo"
+then grep -ri -E "$SEARCHREGEX" **/*.{tex,csv} > "$(printf '%(%Y-%m-%d)T\n' -1).todo"
 exit
 fi
 
-TODOS=$(grep -ril -e 'todo' **/*.{tex,csv,tpl,lua})
+TODOS=$(grep -ril -E "$SEARCHREGEX" **/*.{tex,csv,tpl,lua})
 if [ "$1" = "open" ]
 then for i in $TODOS
     do start $i
