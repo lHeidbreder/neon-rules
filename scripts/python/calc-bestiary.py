18fb1706ab6308e6aa98e372c4430bbf0603f703
lHeidbreder <lukas@boeke.email>
2025-04-14 13:15:04 +0200

    bestiary update
    skills and ratings fixed for gm-companion bestiary

--- scripts/python/calc-bestiary.py
+++ scripts/python/calc-bestiary.py
@@ -15,6 +15,7 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument('file', type=Path)
 parser.add_argument('-v','--debug',action='store_true')
+parser.add_argument('-o','--outfile',type=Path)
 args = parser.parse_args()
 #endregion
 
@@ -22,10 +23,40 @@ args = parser.parse_args()
 combat_divisor = 20
 social_divisor = 3
 
-known_boons = None
-known_banes = None
-known_abilities = None
-known_traits = None
+combat_skills = ['(?:armed )?(?:Ranged|Melee)? Combat Trainings?', 'Dodge', 'Perception', 'Restraint', 'Stealth', 'Wirewalk']
+social_skills = ['Education: ', 'Appeal', 'Deception', 'Estimation', 'Interrogation', 'Intimidate', 'Intimidation', 'Logic', 'Restraint']
+positive_traits = []
+negative_traits = []
+
+def sum_up_entry(list, entry):
+    # Assume list is one of the above
+    cat_entry = as_list(entry)
+    if cat_entry in ('', "None"):
+        return 0
+    dbgprint(cat_entry)
+
+    rtn = 0
+    for e in cat_entry:
+        for x in list:
+            if (match := re.search(r'\b' + x + r'\s+\b(\w+)\b', e, re.I)):
+                qualifier = match.group(1)
+            else:
+                continue
+            qualifier = qualifier_to_levels(qualifier)
+            
+            if (match := re.search(r'\b(\w+)\b\s+' + x, e, re.I)):
+                quantifier = match.group(1)
+            else:
+                quantifier = "one"
+            if quantifier:
+                if not number_words[quantifier.lower()]:
+                    raise NotImplementedError('Unknown number word:', quantifier)
+                quantifier = number_words[quantifier.lower()]
+
+            dbgprint(f"*{quantifier}* {e} *{qualifier}*")
+
+            rtn += qualifier * quantifier
+    return rtn
 
 #region UTILS
 def dbgprint(v):
@@ -96,13 +127,33 @@ def reader_by_doc_type(fhandle, type):
             dbgprint("Is CSV")
             return csv.DictReader(fhandle)
     raise TypeError(f"Is unknown file type: {type}")
+
+def as_list(entry) -> List:
+    if isinstance(entry, str):
+        return entry.split(';')
+    if isinstance(entry, List):
+        return entry
+    raise NotImplementedError('Unhandled type:', type(entry))
+
+number_words = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+}
+def qualifier_to_levels(qualifier: str) -> int:
+    match qualifier.lower():
+        case "known" | "familiar": return 1
+        case "trained" | "informed": return 2
+        case "experienced" | "knowledgable": return 3
+        case "mastered": return 4
+    return 0
 #endregion
 
+header = ("Name", "Social", "Current", "Combat", "Current")
+rtn = [header]
 with open(args.file) as fhandle:
     reader = reader_by_doc_type(fhandle, Path(args.file).suffix)
     
-    header = ("Name", "Social", "Current", "Combat", "Current")
-    rtn = [header]
     for line in reader:
         dbgprint(line['name'])
         if line['name'].startswith('%'):
@@ -111,7 +162,9 @@ with open(args.file) as fhandle:
 
         #social
         characteristics = (parseint(line['cr']) + parseint(line['ch']))/10 #TODO: add instinct?
-        skills = parseint(line['sum:social_skills']) #TODO: get dynamically?
+        #skills = parseint(line['sum:social_skills']) #TODO: get dynamically?
+        skills = sum_up_entry(social_skills, line['itemize:skills']) if line['sum:social_skills'] in ('',None) else parseint(line['sum:social_skills'])
+        dbgprint(f"Social Skills: {skills}")
         social = (characteristics+skills)/social_divisor
 
         #combat
@@ -119,9 +172,10 @@ with open(args.file) as fhandle:
         dbgprint(f"Defenses: {defenses}")
         characteristics = ( parseint(line['cr']) + parseint(line['int']) + parseint(line['ins']) + parseint(line['dex']) + parseint(line['str']) )/5
         dbgprint(f"Characterisitics: {characteristics}")
-        skills = 5 * parseint(line['sum:combat_skills']) + parseint(line['avg_raw_dmg'])
-        dbgprint(f"Skills: {skills}")
-        traits = parseint(line['positive_traits-negative_traits'])/10 + 1 #TODO: can be negative?
+        skills = 5 * sum_up_entry(combat_skills, line['itemize:skills']) + parseint(line['avg_raw_dmg'])
+        #skills = 5 * parseint(line['sum:combat_skills']) + parseint(line['avg_raw_dmg']) #TODO: get dynamically?
+        dbgprint(f"Combat Skills: {skills}")
+        traits = parseint(line['positive_traits-negative_traits'])/10 + 1 #TODO: can be negative? #TODO: get dynamically?
         dbgprint(f"Traits: {traits}")
         combat = (defenses + characteristics + skills) * traits / combat_divisor
         dbgprint(f"Combat Rating {combat}")
@@ -135,8 +189,11 @@ with open(args.file) as fhandle:
             ))
         dbgprint("\n=======\n")
     
-    table = Texttable(120)
-    table.set_cols_align( ("l","r","r","r","r") )
-    table.add_rows(rtn)
+table = Texttable(120)
+table.set_cols_align( ("l","r","r","r","r") )
+table.add_rows(rtn)
 
-    print(table.draw())
+if args.outfile is not None:
+    with open(args.outfile, 'w') as fhandle:
+        fhandle.write(table.draw())
+else: print(table.draw())
