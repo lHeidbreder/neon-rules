393d1c01a9bdb3e680639ae5423d2b744b62870e
lHeidbreder <lukas@boeke.email>
2026-01-12 11:26:13 +0100

    line ending renorm, removed obsolete csv file

--- core-rulebook/prep.sh
+++ core-rulebook/prep.sh
@@ -3,5 +3,3 @@ python ../../scripts/python/split_csv.py armor.csv group
 python ../../scripts/python/split_csv.py meleecomponents.csv type
 cd ../prefabs/
 python ../../scripts/python/split_json.py prefab-stats.json name
-cd ../lists/
-python ../../scripts/python/split_csv.py bodylocations.csv body
